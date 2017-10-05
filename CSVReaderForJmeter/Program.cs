41235af0f9413270a4ea538ae90885b8fe2733cd
miha_vinichenko <mykhailo.vinichenko@wikrgroup.com>
2017-10-05 17:10:01 +0300

    add output file with percents of failed responses; validator changed

--- CSVReaderForJmeter/Program.cs
+++ CSVReaderForJmeter/Program.cs
@@ -7,6 +7,7 @@ namespace CSVReaderForJmeter
 {
     class Program
     {
+        private static double _errorPercent;
         static int Main(string[] args)
         {
             string userPath;
@@ -30,9 +31,7 @@ namespace CSVReaderForJmeter
             columnsContent.RemoveAt(columnsContent.Count - 1);
             columnsContent.RemoveRange(0, headersCount);
             Dictionary<string, string[]> normalizedCSV = WriteCsvToDictionary(columnHeaders, columnsContent.ToArray());
-            if (!ValidateErrorMessages(normalizedCSV))
-                Environment.Exit(1);
-            if (!ValidateStatus(normalizedCSV))
+            if (!ValidateErrors(normalizedCSV, 1))
                 Environment.Exit(1);
             return 0;
         }
@@ -76,7 +75,7 @@ namespace CSVReaderForJmeter
         /// <summary>
         /// Get appropriate data from CSV 
         /// </summary>
-        /// <param name="columnsContent"></param>
+        /// <param name="columnsContent">Inner text of column except header</param>
         /// <param name="firstColumnIndex">Index of the top element in column</param>
         /// <param name="headerLenght">Count of headers</param>
         /// <returns>Appropriate column content, based on index</returns>
@@ -94,6 +93,7 @@ namespace CSVReaderForJmeter
         /// <summary>
         /// Check if the 'Failure Message' column contains any message
         /// </summary>
+        [System.Obsolete("ValidateErrorMessages is deprecated, please use ValidateErrors instead")]
         private static bool ValidateErrorMessages(Dictionary<string, string[]> columnsFromFile)
         {
             string[] errors = columnsFromFile["failureMessage"];
@@ -107,10 +107,64 @@ namespace CSVReaderForJmeter
         /// <summary>
         /// Check if the 'Satus' column contains failed records
         /// </summary>
+        [System.Obsolete("ValidateStatus is deprecated, please use ValidateErrors instead")]
         private static bool ValidateStatus(Dictionary<string, string[]> columnsFromFile)
         {
             string[] errors = columnsFromFile["success"];
             if (errors.Any(t => t.ToLower().Trim() != "true"))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Check the percent of server and client errors from test run
+        /// </summary>
+        /// <param name="countOfResponses">The full count of received responses</param>
+        /// <param name="errorCodes">List of all selected error codes</param>
+        private static void ValidateErrorPercents(int countOfResponses, List<string> errorCodes)
+        {
+            List<string> serverErrorsList = errorCodes.Select(el => el).Where(el => el.StartsWith("5")).ToList();
+            List<string> clientErrorsList = errorCodes.Select(el => el).Where(el => el.StartsWith("4")).ToList();
+            string percentOfServerErrors = null;
+            string percentOfClientErrors = null;
+            double serverErrors = 0;
+            double clientErrors = 0;
+
+            if (serverErrorsList.Count > 0)
+            {
+                serverErrors = serverErrorsList.Count * 100.0/countOfResponses;
+                serverErrors = Math.Round(serverErrors, 2);
+                percentOfServerErrors = $"The test run finished with { serverErrors } % of server errors";
+            }
+            if (serverErrorsList.Count > 0)
+            {
+                clientErrors = clientErrorsList.Count * 100/countOfResponses;
+                clientErrors = Math.Round(clientErrors, 2);
+                percentOfClientErrors = $"The test run finished with { clientErrors } % of client errors";
+            }
+            _errorPercent = serverErrors + clientErrors;
+            File.WriteAllText($"{Directory.GetCurrentDirectory()}\\TestRunErrorInfo.txt", $"{percentOfServerErrors}\r\n{percentOfClientErrors}");
+        }
+
+        /// <summary>
+        /// Check the percent of error status codes from dictionary 
+        /// </summary>
+        /// <param name="columnsFromFile">Target dictionary wiht 'responseCode' column</param>
+        /// <param name="acceptableErrorPercent">Acceptable percent of failed responses</param>
+        /// <returns>Return false if actual percent of error responses more than acceptable</returns>
+        private static bool ValidateErrors(Dictionary<string, string[]> columnsFromFile, double acceptableErrorPercent)
+        {
+            string[] responses = columnsFromFile["responseCode"];
+            List<string> errorsList = responses.Select(el => el).Where(el => el.StartsWith("5") || el.StartsWith("4")).ToList();
+
+            if (errorsList.Count == 0)
+            {
+                return true;
+            }
+            ValidateErrorPercents(responses.Length, errorsList);
+            if (_errorPercent > acceptableErrorPercent)
             {
                 return false;
             }
